750593c7e0fdb0a0a3d2c08b20062a5f20c7de18
martinpinto <martinpintob@gmail.com>
2018-08-28 16:50:23 +0200

    Added sample pages and routes for dashboard.

--- client/src/app/views/main/main.module.ts
+++ client/src/app/views/main/main.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
+import { NbSidebarModule, NbLayoutModule, NbSidebarService, NbCardModule } from '@nebular/theme';
+import { ChartModule } from 'angular2-chartjs';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AnalyticsComponent } from './dashboard/analytics/analytics.component';
+import { JobsComponent } from './dashboard/jobs/jobs.component';
+import { SettingsComponent } from './dashboard/settings/settings.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent,
+    children: [
+      { path: 'jobs', component: JobsComponent },
+      { path: 'analytics', component: AnalyticsComponent },
+      { path: 'settings', component: SettingsComponent }
+    ]
+  }
 ];
 
 @NgModule({
@@ -16,13 +26,18 @@ const routes: Routes = [
     CommonModule,
     NbLayoutModule,
     NbSidebarModule,
-    RouterModule.forRoot(routes)
+    NbCardModule,
+    RouterModule.forRoot(routes),
+    ChartModule
   ],
   exports: [
     RouterModule
   ],
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    AnalyticsComponent,
+    JobsComponent,
+    SettingsComponent
   ],
   providers: [
     NbSidebarService
